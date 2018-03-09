fc30486186bdeac45bd0329193b82912c25e440c
Noah Strayve <noahstrayve@gmail.com>
2018-03-09 16:19:40 -0600

    ITSA BURGER TIME

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name, eaten) VALUES ('Big Kahuna Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Goodburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baby You Can Chive My Car Burger  (comes w/ chives)', true);
+INSERT INTO burgers (name, eaten) VALUES ('Krusty Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('White Castle Slider', true);
+INSERT INTO burgers (name, eaten) VALUES ('Jucy Lucy', false);
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,57 +1,57 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-state").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var consumedStatus = $(this).data("munch-status");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var isEaten = {
+      eaten: consumedStatus
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: isEaten
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", consumedStatus);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".create-burger").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      eaten: $("[name=eaten]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE",
     }).then(
       function() {
-        console.log("deleted cat", id);
+        console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
